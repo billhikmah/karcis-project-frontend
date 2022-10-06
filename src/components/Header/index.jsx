ebebcf81dadbfc3b3400fd717c28ecf55ee794d8
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-10-07 05:48:01 +0700

    debugging

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -24,10 +24,7 @@ function index() {
       setProfilePicture(result.data.data[0].image);
       setProfileName(result.data.data[0].name);
     } catch (error) {
-<<<<<<< HEAD
       // eslint-disable-next-line no-console
-=======
->>>>>>> 04549a6732fd8d4e140100d827383e0d075f2e73
       console.log(error);
       navigate("/*");
     }
