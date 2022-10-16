3bb73345030f01e127c8fbfd47f678b1c97811fb
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-10-17 06:28:08 +0700

    Add redux setup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import redux from "./redux";
+const { store, persistor } = redux;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
